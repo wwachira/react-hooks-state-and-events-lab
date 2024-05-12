79dc3cb018f59d05b117df956860998e5767d669
lizwachira <lizwanguwachira@gmail.com>
2024-05-12 20:39:46 +0300

    feat: filter works

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  
   const appClass = false ? "App dark" : "App light"
 
   return (
